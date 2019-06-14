4c42c368852ca996d2ae1f67f886d45b31e1436e
Mano Sriram <mano.sriram0@gmail.com>
2019-06-14 20:09:48 +0530

    Added Notebook

--- selectSeries.py
+++ selectSeries.py
@@ -5,5 +5,26 @@ ufo = pd.read_csv("http://bit.ly/uforeports")
 
 # City      Colors Reported             Shape Reported          State               Time
 
-ufo['Location'] = ufo['City'] + ufo['State']
-print(ufo['Location'])
+# ufo['Location'] = ufo['City'] + ufo['State']
+
+# print(ufo['Location'].head())
+
+# Rename Columns
+
+# ufo_cols = ["city", "colors reported",
+# "shape reported", "state", "time", "location"]
+
+# ufo.rename(columns={
+#     'Colors Reported': 'Color_Reported',
+#     'Shape Reported': 'Shape_Reported'
+# }, inplace=True)
+
+# OR
+
+# ufo.columns = ufo_cols
+
+# OR
+
+ufo.columns = ufo.columns.str.replace(" ", "_")
+
+print(ufo.head())
